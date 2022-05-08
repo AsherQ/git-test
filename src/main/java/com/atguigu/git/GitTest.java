7e1c435433496d471b97abe47e272111cf81f86c
ljw <1370130898@qq.com>
2022-05-08 16:59:37 +0800

    gitee test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("master commit !!!");
         System.out.println("hot-fix test!!!!");
         System.out.println("hot-fix test!!!!");
+        System.out.println("gitee test!!!!");
     }
 
 }
