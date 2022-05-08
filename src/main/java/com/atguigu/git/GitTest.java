9e41f1e32cdc662ec5f3167e6b735c29714476b2
ljw <1370130898@qq.com>
2022-05-08 16:10:46 +0800

    master commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,8 +4,9 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git2!!");
-        System.out.println("hello git2!!!");
+        System.out.println("hello git3!!!");
         System.out.println("hello git4!!!");
+        System.out.println("master commit !!!");
     }
 
 }
